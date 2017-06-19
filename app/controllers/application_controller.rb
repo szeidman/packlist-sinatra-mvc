fe19534589e3fd6caede20baad2403d0785d5092
szeidman <sam.zeidman@gmail.com>
2017-06-19 16:15:31 -0400

    Seperate User controller and views (with app controller saved)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,51 +14,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get "/signup" do
-    erb :signup
-  end
-
-  post "/signup" do
-    if params[:username].empty? || params[:password].empty?
-      redirect '/failure'
-    else
-      User.create(params)
-      redirect '/signin'
-    end
-  end
-
-  get "/account" do
-    @user = User.find(session[user_id])
-    erb :account
-  end
-
-  get "/signin" do
-    user = User.find_by(username: params[:username])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "/account"
-      else
-        redirect "/failure"
-      end
-  end
-
-  get "/success" do
-    if logged_in?
-      erb :success
-    else
-      redirect "/signin"
-    end
-  end
-
-  get "/failure" do
-    erb :failure
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
   helpers do
     def logged_in?
       !!session[:user_id]
