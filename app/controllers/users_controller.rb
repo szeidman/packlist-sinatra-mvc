f5a8d105db21568f833ea2d84bbfb76c3f9e9674
szeidman <sam.zeidman@gmail.com>
2017-06-19 18:28:58 -0400

    Change bikes to panniers, populate models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/signin'
     else
-      redirect '/bikes'
+      redirect '/panniers'
     end
   end
 
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect "/bikes"
+        redirect "/panniers"
       else
         redirect "/signup"
       end
