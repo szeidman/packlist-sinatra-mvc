b2ac93d25b463b61b13024d713f36bcca2c4b401
szeidman <sam.zeidman@gmail.com>
2017-06-19 14:07:57 -0400

    Populate MVC folders, create index and layout

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views")}
+  register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/' do
+    erb :index
+  end
+end
