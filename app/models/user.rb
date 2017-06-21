632b5bef6e909b0650df2b469e38384c777f3b11
szeidman <sam.zeidman@gmail.com>
2017-06-20 21:53:03 -0400

    Add validations to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :panniers
   has_many :items, through: :panniers
+
+  validates :username, presence: true
+  validates :password_digest, presence: true
+  validates_associated :items
+  validates_associated :panniers
+
 end
