75c782cb8e71e7efe225307d3ec42d862b811811
szeidman <sam.zeidman@gmail.com>
2017-06-21 16:03:42 -0400

    Reenable validations, move pannier delete button

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :pannier
 
-#  validates :name, presence: true
-#  validates :weight, numericality: true
-#  validates_associated :items
-#  validates_associated :panniers
+  validates :name, presence: true
+  validates :weight, numericality: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
   has_many :panniers
   has_many :items, through: :panniers
 
-#Í  validates :username, presence: true
-#Í  validates :password_digest, presence: true
-#Í  validates_associated :items
-#Í  validates_associated :panniers
+  validates :username, presence: true
+  validates :password_digest, presence: true
 
 end
