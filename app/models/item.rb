cffd700837f5d345c6cb98d73475632bfd7db90e
szeidman <sam.zeidman@gmail.com>
2017-06-20 21:55:02 -0400

    Add validations to item class

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :pannier
+
+  validates :name, presence: true
+  validates :weight, numericality: true
+  validates_associated :items
+  validates_associated :panniers
 end
